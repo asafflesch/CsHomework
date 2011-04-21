117e7bb9cd3e883af2e8423be8401a5aa91cfaa0
Asaf Flescher <asafflesch@gmail.com>
2011-04-21 22:23:32 +0300

    added new files:

--- StringComparator.java
+++ StringComparator.java
@@ -6,7 +6,26 @@ public class StringComparator implements Comparator {
 
 	@Override
 	public int compare(Object o1, Object o2) {
-		return 0;
+                String str1 = o1.toString(), str2 = o2.toString();
+
+                int minLength = str1.length(), ret = 0;
+                if (str2.length() < minLength) minLength = str2.length();
+
+                for (int i = 0; i < minLength && ret == 0; ++i)
+                {
+                    if (str1.charAt(i) < str2.charAt(i))
+                        ret = -1;
+                    else if (str1.charAt(i) > str2.charAt(i))
+                        ret = 1;
+                }
+
+                if (ret == 0)
+                {
+                    if (str2.length() > minLength) ret = 1;
+                    else if (str1.length() > minLength) ret = -1;
+                }
+
+		return ret;
 	}
 
 }
